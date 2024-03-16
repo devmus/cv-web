73db14a78e78a943a42316495661ac9423853ba3
Rasmus <rasmus.wersall@gmail.com>
2024-03-16 19:19:03 +0100

    :bricks: Update src links for deployment

--- src/components/PageOne/Bild/Bild.jsx
+++ src/components/PageOne/Bild/Bild.jsx
@@ -4,7 +4,7 @@ import "./bild.css"
 export const Bild = () => {
   return (
     <div className="image">
-      <img src="./src/assets/rasmus2.jpg" alt="Rasmus Wersäll profilbild" />
+      <img src="./assets/rasmus2.jpg" alt="Rasmus Wersäll profilbild" />
     </div>
 
   )
